2c730243b16245a4d862d19ad307cb10b85628da
diegorezm <dmourarezende@gmail.com>
2024-11-06 17:13:41 -0300

    env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,3 @@
 export const environment = {
   production: false,
-  API_URL: import.meta.env["NG_APP_PUBLIC_API_URL"]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
   production: true,
-  API_URL: import.meta.env["NG_APP_PUBLIC_API_URL"]
 };
